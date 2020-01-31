b1169cd5d89e21f9629ad048754e5c487a65aecc
Avrik Berman <avrik@cyesec.com>
2020-01-31 22:30:24 +0200

    what what

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 
 
 let open = true;
+let dropped = 0;
 
+let readyForRearange = false;
 const inventoryItems = [
-    { id: '1', name: 'milk', src: './assets/milk.png' },
+    { id: '1', name: 'cupcake', src: './assets/cupcake.png' },
     { id: '2', name: 'pitcher', src: './assets/pitcher.png' },
     { id: '3', name: 'eggs', src: './assets/eggs_01.png' },
-    { id: '4', name: 'milk', src: './assets/milk.png' },
-    { id: '5', name: 'milk', src: './assets/milk.png' },
+    { id: '4', name: 'buttermilk', src: './assets/buttermilk.png' },
+    { id: '5', name: 'mystery', src: './assets/mystery.png' },
 ]
+
+const inventoryDivItems = [];
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -25,48 +29,80 @@ function drop(ev) {
     const element = document.getElementById(data);
     element.style.opacity = 1;
     ev.target.appendChild(element);
+
+    dropped++;
+
+    document.querySelector('.instructions').style.opacity = dropped >0 ? 0 : 1;
+    document.querySelector('.ready-btn').style.opacity = dropped >= 5 ? 1 : 0;
+
+    
+}
+
+function getOpacity() {
+    return 0;
 }
 
 function readyClicked() {
 
     open = !open;
     // const str = open ? 'open' : 'close'
+    const dropZonesContainer = document.querySelector('.drop-zones-container');
     const fridge = document.querySelector('.refregirator');
-    const fridgeContent = document.querySelector('.drop-zone');
+    const fridgeImg = document.querySelector('#fridge');
 
     if (fridge.classList.contains('bounceInRight')) {
-        fridge.classList.remove('animated','bounceInRight');
-        console.log('VBBDFBDFGD');
-        
+        fridge.classList.remove('animated', 'bounceInRight');
     }
 
-    fridgeContent.style.opacity = open ? 1 :0;
+    dropZonesContainer.style.opacity = open ? 1 : 0;
 
     if (!open) {
-        fridge.classList.add('animated', 'infinite', 'shake', 'delay-500ms');
+        fridgeImg.style['margin-left'] = '0';
+        fridgeImg.classList.add('animated', 'infinite', 'shake', 'delay-500ms');
+        document.querySelector('.ready-btn').innerHTML = "STOP";
+        readyForRearange = true;
     } else {
-        fridge.classList.remove('animated', 'infinite', 'shake', 'delay-500ms');
+        fridgeImg.style['margin-left'] = '200px';
+        fridgeImg.classList.remove('animated', 'infinite', 'shake', 'delay-500ms');
+
+        if (readyForRearange) {
+            document.querySelector('.ready-btn').style.opacity = 0;
+            
+            inventoryDivItems.forEach(item=>{
+                fridge.appendChild(item);
+                
+            })
+        }
     }
 
-    document.getElementById("fridge").src = open?'./assets/openedFridge_02.png':'./assets/closedFridge_02.png';
+
+
+    fridgeImg.src = open ? './assets/openedFridge_02.png' : './assets/closedFridge_02.png';
+
+
 }
 
 
 function init() {
+
     setTimeoutAchievements();
-    /* const canvas = document.getElementById('main-canvas');
-    const ctx = canvas.getContext('2d');
 
-    console.log(ctx); */
+    const dropzone = document.querySelector('#dropzone');
 
-
-    // const dropzone = document.getElementById('dropzone');
     const item = document.getElementById('dpz-item');
     const totalSlots = 15;
     for (let i = 0; i < totalSlots; i++) {
         let element = item.cloneNode(true);
-        item.parentNode.appendChild(element);
+        dropzone.appendChild(element);
     }
+
+    const btn = document.querySelector('.ready-btn')
+    btn.style.opacity = 0;
+    /*  const dropzone2 = document.querySelector('#dropzone2');
+     for (let i = 0; i < 9; i++) {
+         let element = item.cloneNode(true);
+         dropzone2.appendChild(element);
+     } */
 
     item.parentNode.removeChild(item);
 
@@ -75,14 +111,15 @@ function init() {
         const invItem = document.createElement("div");
         const img = document.createElement("img");
 
+        inventoryDivItems.push(invItem);
         img.src = item.src;
         img.height = 60;
-        img.style.pointerEvents ='none';
+        img.style.pointerEvents = 'none';
         invItem.appendChild(img);
         invItem.className = "inventory-item";
         invItem.draggable = true;
         invItem.id = item.id;
-        invItem.ondragexit = (ev) =>{
+        invItem.ondragexit = (ev) => {
             ev.target.style.opacity = 1;
         }
         invItem.ondragstart = (ev) => {
